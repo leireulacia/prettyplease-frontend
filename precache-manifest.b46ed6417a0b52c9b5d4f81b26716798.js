a197b955545bcc4fc31a0349fd20545ba8fef308
Mark L <19728298+M-Lieberman@users.noreply.github.com>
2020-10-15 15:54:03 +0100

    Updates

---
+++ precache-manifest.b46ed6417a0b52c9b5d4f81b26716798.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c6791214054d79fd042e21540054e9",
+    "url": "/prettyplease-frontend/index.html"
+  },
+  {
+    "revision": "4bdda756e5947c0ca54b",
+    "url": "/prettyplease-frontend/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2a1e46417ca799319150",
+    "url": "/prettyplease-frontend/static/css/main.067410c7.chunk.css"
+  },
+  {
+    "revision": "4bdda756e5947c0ca54b",
+    "url": "/prettyplease-frontend/static/js/2.aad614a8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/prettyplease-frontend/static/js/2.aad614a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a1e46417ca799319150",
+    "url": "/prettyplease-frontend/static/js/main.c2f14ded.chunk.js"
+  },
+  {
+    "revision": "c07f7f86c38865880859",
+    "url": "/prettyplease-frontend/static/js/runtime-main.c638165f.js"
+  },
+  {
+    "revision": "aed2ebbe7dc1221c21a7fb8b900cdbc5",
+    "url": "/prettyplease-frontend/static/media/Jean-Seberg.aed2ebbe.jpg"
+  },
+  {
+    "revision": "c12ae83accb90a5eb1262a97f694285b",
+    "url": "/prettyplease-frontend/static/media/abstract-logo.c12ae83a.jpg"
+  },
+  {
+    "revision": "61e6e747a6d4f4656b161b3b69caad60",
+    "url": "/prettyplease-frontend/static/media/abstract-logo2.61e6e747.jpg"
+  },
+  {
+    "revision": "d4d0f2c9209822b1c7c87eca90f5d293",
+    "url": "/prettyplease-frontend/static/media/abstract-logo3.d4d0f2c9.jpg"
+  },
+  {
+    "revision": "053a4a7437b3ce5ab0e04289811359db",
+    "url": "/prettyplease-frontend/static/media/abstract-logo4.053a4a74.jpg"
+  },
+  {
+    "revision": "4c725eeb1778a9de615f856c6a63ce19",
+    "url": "/prettyplease-frontend/static/media/charity-logo-orange.4c725eeb.png"
+  }
+]);
